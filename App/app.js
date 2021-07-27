31dd6fe2c2f48c7b78aa0c30ada7e5609d7577c2
Ramansh Chhabra <best_ramansh@hotmail.com>
2021-07-27 07:42:59 -0700

    start jss, add event listener to new budget button, change syntax upon clicking

---
+++ App/app.js
@@ -0,0 +1,29 @@
+const budgetList = []
+
+
+const addBtn = document.querySelector('#new-button')
+const budgetArea = document.querySelector('.budget-area')
+
+
+addBtn.addEventListener('click', ()=>{
+    const title = document.querySelector('#expense-info').value
+    const amount = parseFloat(document.querySelector('#expense-amount').value)
+
+    balance = amount
+
+    const budgetPage = `
+    <button id="all-budgets">All budgets</button>
+    <h1>Budget for: ${title}</h1>
+    <h2>Monthly budget: $${amount}</h2>
+    <ul class="budget-items">
+                
+    </ul>
+    <div class="create-budget-item">
+        <input type="text" name="new-expense" id="new-expense" placeholder="Add new expense">
+        <input type="text" name="expense-amount" id="expense-amount" placeholder="Amount($)">
+        <button id="new-button">Add</button>
+    </div>
+    <h3>Remaining balance: $<span id="remaining-balance"></span><h3>  `
+
+    const modify = document.querySelector('.budget-area')
+    modify.innerHTML = budgetPage
